889f68cabc8e35370c13767c596f5d435319c398
Matheus Teles <matheustdo1@gmail.com>
2022-02-13 20:00:43 -0300

    Edit colors scheme

--- src/constants/schemes.jsx
+++ src/constants/schemes.jsx
@@ -1,37 +1,53 @@
 export const lightScheme = {
-  primary: "#0062FF",
-  secondary: "#DA0037",
-  orange: "#FF974A",
-  yellow: "#FFC542",
-  blue: "#6AC0FF",
-  ocean: "#0062FF",
-  red: "#FC5A5A",
+  primary: "#FF974A",
+  secondary: "#6AC0FF",
+  pallete: {
+    orange: "#FF974A",
+    yellow: "#FFC542",
+    baby: "#6AC0FF",
+    ocean: "#0062FF",
+    olive: "#06BC82",
+    salmon: "#FC5A5A",
+    magenta: "#D7175C",
+  },
   background: {
     main: "#F5F6FA",
     secondary: "#FFFFFF",
     tertiary: "#E9EAF0",
   },
   text: {
-    primary: "#0e1e25",
-    secondary: "#646e73",
+    primary: "#16191C",
+    secondary: "#696973",
+  },
+  stroke: {
+    soft: "#F2F2F2",
+    hard: "#E3E3E3",
   },
 };
 
 export const darkScheme = {
-  primary: "#6AC0FF",
-  secondary: "#DA0037",
-  orange: "#FF974A",
-  yellow: "#FFC542",
-  blue: "#6AC0FF",
-  ocean: "#0062FF",
-  red: "#FC5A5A",
+  primary: "#FF974A",
+  secondary: "#6AC0FF",
+  pallete: {
+    orange: "#FF974A",
+    yellow: "#FFC542",
+    baby: "#6AC0FF",
+    ocean: "#0062FF",
+    olive: "#06BC82",
+    salmon: "#FC5A5A",
+    magenta: "#D7175C",
+  },
   background: {
-    main: "#16191C",
-    secondary: "#1F2327",
-    tertiary: "#292E33",
+    main: "#13131A",
+    secondary: "#1C1C24",
+    tertiary: "#15181C",
   },
   text: {
     primary: "#FBFBFC",
-    secondary: "#696973",
+    secondary: "#8B8B94",
+  },
+  stroke: {
+    soft: "#2A2D33",
+    hard: "#28292B",
   },
 };
